6bde7b9ab76f23db46c7818a74386351cd0e08bf
Shuai Zhang <zhangshuai.ustc@gmail.com>
2025-07-18 01:09:05 -0700

    chore(editor): Migrate to Use SDK-Style csproj

--- ImageOcclusionEditor/MainForm.Designer.cs
+++ ImageOcclusionEditor/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ImageOcclusionEditor
+namespace ImageOcclusionEditor
 {
   partial class MainForm
   {
@@ -28,76 +28,82 @@
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-      this.wb = new System.Windows.Forms.WebBrowser();
-      this.btnSaveExit = new System.Windows.Forms.Button();
-      this.btnSave = new System.Windows.Forms.Button();
-      this.btnCancel = new System.Windows.Forms.Button();
-      this.SuspendLayout();
-      // 
-      // wb
-      // 
-      this.wb.AllowNavigation = false;
-      this.wb.AllowWebBrowserDrop = false;
-      this.wb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.btnSaveExit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wb
+            // 
+            this.wb.AllowNavigation = false;
+            this.wb.AllowWebBrowserDrop = false;
+            this.wb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-      this.wb.Location = new System.Drawing.Point(1, 1);
-      this.wb.MinimumSize = new System.Drawing.Size(20, 20);
-      this.wb.Name = "wb";
-      this.wb.ScriptErrorsSuppressed = true;
-      this.wb.ScrollBarsEnabled = false;
-      this.wb.Size = new System.Drawing.Size(1062, 636);
-      this.wb.TabIndex = 0;
-      // 
-      // btnSaveExit
-      // 
-      this.btnSaveExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.btnSaveExit.Location = new System.Drawing.Point(939, 643);
-      this.btnSaveExit.Name = "btnSaveExit";
-      this.btnSaveExit.Size = new System.Drawing.Size(120, 33);
-      this.btnSaveExit.TabIndex = 1;
-      this.btnSaveExit.Text = "Save && E&xit  (Ctrl+S)";
-      this.btnSaveExit.UseVisualStyleBackColor = true;
-      this.btnSaveExit.Click += new System.EventHandler(this.btnSaveExit_Click);
-      // 
-      // btnSave
-      // 
-      this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.btnSave.Location = new System.Drawing.Point(813, 643);
-      this.btnSave.Name = "btnSave";
-      this.btnSave.Size = new System.Drawing.Size(120, 33);
-      this.btnSave.TabIndex = 2;
-      this.btnSave.Text = "&Save  (Ctrl+Shift+S)";
-      this.btnSave.UseVisualStyleBackColor = true;
-      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-      // 
-      // btnCancel
-      // 
-      this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-      this.btnCancel.Location = new System.Drawing.Point(687, 643);
-      this.btnCancel.Name = "btnCancel";
-      this.btnCancel.Size = new System.Drawing.Size(120, 33);
-      this.btnCancel.TabIndex = 3;
-      this.btnCancel.TabStop = false;
-      this.btnCancel.Text = "&Cancel (ESC)";
-      this.btnCancel.UseVisualStyleBackColor = true;
-      this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-      // 
-      // MainForm
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(1064, 681);
-      this.Controls.Add(this.btnCancel);
-      this.Controls.Add(this.btnSave);
-      this.Controls.Add(this.btnSaveExit);
-      this.Controls.Add(this.wb);
-      this.DoubleBuffered = true;
-      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-      this.Name = "MainForm";
-      this.Text = "SuperMemo Image Occlusion Editor";
-      this.ResumeLayout(false);
+            this.wb.Location = new System.Drawing.Point(2, 2);
+            this.wb.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.wb.MinimumSize = new System.Drawing.Size(40, 37);
+            this.wb.Name = "wb";
+            this.wb.ScriptErrorsSuppressed = true;
+            this.wb.ScrollBarsEnabled = false;
+            this.wb.Size = new System.Drawing.Size(2124, 1174);
+            this.wb.TabIndex = 0;
+            // 
+            // btnSaveExit
+            // 
+            this.btnSaveExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSaveExit.Location = new System.Drawing.Point(1878, 1187);
+            this.btnSaveExit.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSaveExit.Name = "btnSaveExit";
+            this.btnSaveExit.Size = new System.Drawing.Size(240, 61);
+            this.btnSaveExit.TabIndex = 1;
+            this.btnSaveExit.Text = "Save && E&xit  (Ctrl+S)";
+            this.btnSaveExit.UseVisualStyleBackColor = true;
+            this.btnSaveExit.Click += new System.EventHandler(this.btnSaveExit_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(1626, 1187);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(240, 61);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "&Save  (Ctrl+Shift+S)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(1374, 1187);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(240, 61);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "&Cancel (ESC)";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2128, 1257);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSaveExit);
+            this.Controls.Add(this.wb);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.Text = "SuperMemo Image Occlusion Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
 
     }
 
