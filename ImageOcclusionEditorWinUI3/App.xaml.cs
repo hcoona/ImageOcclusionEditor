405ebf2ede2f33923ae99fbf6d87785e0d6ccfc5
Shuai Zhang <zhangshuai.ustc@gmail.com>
2025-07-27 22:16:21 -0700

    chore(editor-winui3): Add License Headers for Each Source Code File

--- ImageOcclusionEditorWinUI3/App.xaml.cs
+++ ImageOcclusionEditorWinUI3/App.xaml.cs
@@ -1,3 +1,24 @@
+/*
+ * ImageOcclusionEditorWinUI3 - A WinUI 3 application for creating image occlusion cards
+ * Copyright (C) 2025 Shuai Zhang
+ *
+ * This file contains code derived from ImageOcclusionEditor by SuperMemo Community,
+ * which is licensed under the MIT License.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.IO;
 using System.Linq;
